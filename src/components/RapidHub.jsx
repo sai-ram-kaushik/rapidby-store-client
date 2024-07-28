fddb27b69bd10ec79bcc15acc02ad903cfc921a2
sai-ram-kaushik <sairamkaushik1@gmail.com>
2024-07-28 18:16:32 +0530

    updated with the animation changes

--- src/components/RapidHub.jsx
+++ src/components/RapidHub.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "../utils/Button";
+import { Link } from "react-router-dom";
 
 const RapidHub = () => {
   const images = [
@@ -15,25 +16,30 @@ const RapidHub = () => {
     <div className="w-full px-5 lg:px-10 py-10 bg-primary">
       <div className="flex items-center justify-center w-full">
         <div className="flex flex-col items-center w-full">
-          <h3 className="text-[#CF6FDC] text-[25px] md:text-[38px] font-bold">
+          <h3 className="text-[#CF6FDC] text-[25px] md:text-[38px] font-bold font-heading">
             Rapid hub
           </h3>
           <p>Delivered Door-to-Door</p>
 
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 items-center justify-center gap-4 py-10">
             {images.map((image, index) => (
-              <div key={index} className=" rounded-lg">
+              <div key={index} className="group rounded-lg overflow-hidden">
                 <img
                   src={image}
                   alt={`Product ${index + 1}`}
-                  className="w-full h-auto object-cover rounded-md"
+                  className="w-full h-auto object-cover rounded-md transform transition-transform duration-300 group-hover:scale-125"
                 />
               </div>
             ))}
           </div>
 
           <div>
-            <Button title="See All Products" className="bg-background text-primary text-[20px] font-semibold" />
+            <Link to="/login">
+              <Button
+                title="See All Products"
+                className="bg-background text-primary text-[20px] font-semibold"
+              />
+            </Link>
           </div>
         </div>
       </div>
--- src/components/dashboard/homeComponents/RecentOrders.jsx
+++ src/components/dashboard/homeComponents/RecentOrders.jsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../../../utils/Button";
 import data from "../../../data/Home.json";
+import axios from "axios";
+import moment from "moment-timezone";
+
 
 const RecentOrders = () => {
-  const { orders } = data.orderDetails.ordersList;
+  // const { orders } = data.orderDetails.ordersList;
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_ENDPOINT_URI}/api/store/get-all-orders`)
+      .then((response) => {
+        const sortedOrders = response.data.data.sort(
+          (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+        );
+        setOrders(sortedOrders);
+      })
+      .catch((error) => {
+        console.log("getting error while fetching the order", error);
+      });
+  }, []);
+
+  const formatToIST = (dateString) => {
+    return moment(dateString).tz("Asia/Kolkata").format("DD-MM-YYYY HH:mm:ss");
+  };
 
   return (
     <div className="w-[350px] sm:w-full">
       <div className="flex flex-col items-start bg-background rounded-lg w-full p-5 md:p-8 lg:p-10">
         <div className="flex flex-col md:flex-row items-start md:items-center justify-between w-full mb-5">
-          <h3 className="text-lg md:text-xl lg:text-2xl font-bold">Recent Orders</h3>
+          <h3 className="text-lg md:text-xl lg:text-2xl font-bold">
+            Recent Orders
+          </h3>
           <div className="flex items-center gap-2 md:gap-4 mt-4 md:mt-0">
             <p className="text-sm md:text-base">Filter</p>
             <Button title="Export" />
@@ -36,12 +60,6 @@ const RecentOrders = () => {
                   scope="col"
                   className="px-3 py-2 md:px-6 md:py-3 text-left text-xs md:text-sm font-medium text-gray-500 uppercase tracking-wider"
                 >
-                  Product
-                </th>
-                <th
-                  scope="col"
-                  className="px-3 py-2 md:px-6 md:py-3 text-left text-xs md:text-sm font-medium text-gray-500 uppercase tracking-wider"
-                >
                   Email ID
                 </th>
                 <th
@@ -49,12 +67,6 @@ const RecentOrders = () => {
                   className="px-3 py-2 md:px-6 md:py-3 text-left text-xs md:text-sm font-medium text-gray-500 uppercase tracking-wider"
                 >
                   Order Date/Time
-                </th>
-                <th
-                  scope="col"
-                  className="px-3 py-2 md:px-6 md:py-3 text-left text-xs md:text-sm font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Amount
                 </th>
                 <th
                   scope="col"
@@ -71,25 +83,19 @@ const RecentOrders = () => {
               </tr>
             </thead>
             <tbody className="bg-white divide-y divide-gray-200">
-              {orders.map((order, index) => (
+              {orders.slice(0,4).map((order, index) => (
                 <tr key={index}>
                   <td className="px-3 py-2 md:px-6 md:py-4 whitespace-nowrap text-xs md:text-sm text-gray-700">
-                    {order.orderId}
+                    #{order._id.slice(16, 23)}
                   </td>
                   <td className="px-3 py-2 md:px-6 md:py-4 whitespace-nowrap text-xs md:text-sm text-gray-700">
-                    {order.orderedBy}
-                  </td>
-                  <td className="px-3 py-2 md:px-6 md:py-4 whitespace-nowrap text-xs md:text-sm text-gray-700">
-                    {order.product}
+                    {order.firstName}
                   </td>
                   <td className="px-3 py-2 md:px-6 md:py-4 whitespace-nowrap text-xs md:text-sm text-gray-700">
                     {order.email}
                   </td>
                   <td className="px-3 py-2 md:px-6 md:py-4 whitespace-nowrap text-xs md:text-sm text-gray-700">
-                    {order.orderDate}
-                  </td>
-                  <td className="px-3 py-2 md:px-6 md:py-4 whitespace-nowrap text-xs md:text-sm text-gray-700">
-                    {order.amount}
+                    {formatToIST(order.createdAt)}
                   </td>
                   <td className="px-3 py-2 md:px-6 md:py-4 whitespace-nowrap text-xs md:text-sm text-gray-700">
                     {order.status}
